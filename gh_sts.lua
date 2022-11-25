15b14cef912d7f28d7c84fa582d70806255a7b2d
sudormrfslash <82327900+sudormrfslash@users.noreply.github.com>
2022-11-25 13:44:10 +0000

    Update gh_sts.lua
    character walk-speed modifying added

--- gh_sts.lua
+++ gh_sts.lua
@@ -1,4 +1,5 @@
 local issprinting = false
+local char = script.Parent
 local uis = game:GetService("UserInputService")
 --i don't think the gp parameter is used much don't worry just don't remove it
 --the 'io' parameter is your input object, in simple terms it's what has been registered by userinputservice (could be a mouse click, a keyboard press, etc)
@@ -9,7 +10,7 @@ uis.InputBegan:Connect(function(io, GameProcessed)
 	end
 	if io.KeyCode == Enum.KeyCode.LeftShift then
 		issprinting = true
-		print("sprinting")
+		char.Humanoid.WalkSpeed = (char.Humanoid.WalkSpeed * 1.5)
 	end
 end)
 uis.InputEnded:Connect(function(io, gp)
@@ -18,6 +19,6 @@ uis.InputEnded:Connect(function(io, gp)
 	end
 	if io.KeyCode == Enum.KeyCode.LeftShift then
 		issprinting = false
-		print("not sprinting")
+		char.Humanoid.WalkSpeed = (char.Humanoid.WalkSpeed / 1.5)
 	end
 end)

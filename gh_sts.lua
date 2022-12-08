5a2165e5592775748b5949241add8d447bc72165
sudormrfslash <82327900+sudormrfslash@users.noreply.github.com>
2022-12-08 23:20:33 +0000

    Update gh_sts.lua
    changed a lil error

--- gh_sts.lua
+++ gh_sts.lua
@@ -4,8 +4,8 @@ local uis = game:GetService("UserInputService")
 --i don't think the gp parameter is used much don't worry just don't remove it
 --the 'io' parameter is your input object, in simple terms it's what has been registered by userinputservice (could be a mouse click, a keyboard press, etc)
 
-uis.InputBegan:Connect(function(io, GameProcessed)
-	if GameProcessed then
+uis.InputBegan:Connect(function(io, gp)
+	if gp then
 		return
 	end
 	if io.KeyCode == Enum.KeyCode.LeftShift then
